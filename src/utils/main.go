d2ee79c6a2d8e14d17e881c714dc28230506c4d6
Mohammad Jawad Barati <kasir.barati@gmail.com>
2025-04-16 12:31:39 +0200

    chore: move code to src

--- src/utils/main.go
+++ src/utils/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello, world!")
+	var scores []int = make([]int, 5, 10)
+	fmt.Println(scores[:])   // [0 0 0 0 0]
+	fmt.Println(scores[1:3]) // 5
 }
